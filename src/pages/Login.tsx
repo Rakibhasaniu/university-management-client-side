295c771cd2430d36ed5363d7ce5b39abfce66476
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-01-26 23:09:45 +0600

    Set Cookies In The Browser

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
 
 const Login = () => {
+    const {register,handleSubmit}  =  useForm({
+        defaultValues:{
+            userId:'A-0002',
+            password:'admin123'
+        }
+    })
+
+    const [login,{data,error}] = useLoginMutation()
+
+    console.log('data=>',data)
+    console.log('error=>',error)
+    const onSubmit = (data) => {
+        const userInfo = {
+            id: data.userId,
+            password: data.password
+        }
+
+        login(userInfo)
+    }
     return (
-        <div>
-            <h1>This Is Login Page</h1>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID:</label>
+                <input type="text" id='id' {...register('userId')} />
+            </div>
+            <div>
+                <label htmlFor="password">PASSWORD:</label>
+                <input type="text" id='password' {...register('password')} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
--- src/redux/features/store.ts
+++ src/redux/features/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from '../features/auth/authSlice'
+import { baseApi } from "../api/baseApi";
+
 
 
  export const store = configureStore({
     reducer:{
-        auth: authReducer,
+        [baseApi.reducerPath]: baseApi.reducer,
+        auth: authReducer
     },
+    middleware:(getDefaultMiddlewares) =>getDefaultMiddlewares().concat(baseApi.middleware)
 });
 
 
