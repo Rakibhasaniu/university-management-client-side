4eb06f1cab828fe9fb6d707384a36dea8145e352
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-02-28 23:47:36 +0600

    academic management layout added

--- src/routes/admin.route.tsx
+++ src/routes/admin.route.tsx
@@ -22,41 +22,41 @@ export const adminPaths = [
     path: 'dashboard',
     element: <AdminDashboard />,
   },
-//   {
-//     name: 'Academic Management',
-//     children: [
-//       {
-//         name: 'Create A. Semester',
-//         path: 'create-academic-semester',
-//         element: <CreateAcademicSemester />,
-//       },
-//       {
-//         name: 'Academic Semester',
-//         path: 'academic-semester',
-//         element: <AcademicSemester />,
-//       },
-//       {
-//         name: 'Create A. Faculty',
-//         path: 'create-academic-faculty',
-//         element: <CreateAcademicFaculty />,
-//       },
-//       {
-//         name: 'Academic Faculty',
-//         path: 'academic-faculty',
-//         element: <AcademicFaculty />,
-//       },
-//       {
-//         name: 'Create A. Department',
-//         path: 'create-academic-department',
-//         element: <CreateAcademicDepartment />,
-//       },
-//       {
-//         name: 'Academic Department',
-//         path: 'academic-department',
-//         element: <AcademicDepartment />,
-//       },
-//     ],
-//   },
+  {
+    name: 'Academic Management',
+    children: [
+      {
+        name: 'Create A. Semester',
+        path: 'create-academic-semester',
+        element: <CreateAcademicSemester />,
+      },
+      {
+        name: 'Academic Semester',
+        path: 'academic-semester',
+        element: <AcademicSemester />,
+      },
+      {
+        name: 'Create A. Faculty',
+        path: 'create-academic-faculty',
+        element: <CreateAcademicFaculty />,
+      },
+      {
+        name: 'Academic Faculty',
+        path: 'academic-faculty',
+        element: <AcademicFaculty />,
+      },
+      {
+        name: 'Create A. Department',
+        path: 'create-academic-department',
+        element: <CreateAcademicDepartment />,
+      },
+      {
+        name: 'Academic Department',
+        path: 'academic-department',
+        element: <AcademicDepartment />,
+      },
+    ],
+  },
   {
     name: 'User Management',
     children: [
