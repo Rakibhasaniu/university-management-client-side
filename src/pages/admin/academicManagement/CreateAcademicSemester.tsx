95d44eff01f67fc1004bd87d36f225c1443b9291
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-03-03 17:45:01 +0600

    bug solved

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,5 +1,5 @@
 import PHForm from "@/components/form/UniForm";
-import PHInput from "@/components/form/UniInput";
+// import PHInput from "@/components/form/UniInput";
 import PHSelect from "@/components/form/UniSelect";
 import { Button, Col, Flex } from "antd";
 import { FieldValues, SubmitHandler } from "react-hook-form";
@@ -13,8 +13,8 @@ const CreateAcademicSemester = () => {
         <Flex justify="center" align="center">
         <Col span={6}>
         <PHForm onSubmit={onSubmit}>
-            <PHInput type="text" name="name" label="Name"/>
-            <PHInput type="text" name="year" label="Year"/>
+            {/* <PHInput type="text" name="name" label="Name"/>
+            <PHInput type="text" name="year" label="Year"/> */}
             <PHSelect label="name"/>
             <Button htmlType="submit">Submit</Button>
         </PHForm>
